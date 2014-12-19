49e3623b856e457cd3e77a279218a0a4a3da1016
Stefan <stefan.guesten@gmx.de>
2014-12-19 14:26:28 +0100

    gitignore erweitert

--- src/main/java/text_highlighter/distance_calculate.java
+++ src/main/java/text_highlighter/distance_calculate.java
@@ -3,16 +3,16 @@ package text_highlighter;
 public class distance_calculate {
 
     public static void main(final String[] args) {
-    	System.out.println("START - readTitles");
-    	
-    	String[] titles = ReadTitles.readTitles("./data/all_title.xml");
-    	String places = "./data/all_places.xml";
-    	String outputFile = "./data/places_highlighted.xml";
-    	
-    	System.out.println("START - ReadPlacesCompareAndWrite");
-    	
-    	ReadPlacesCompareAndWrite.readPlacesCompareAndWrite(places, titles, outputFile);
-    	
-    	System.out.println("Finish");
+        System.out.println("START - readTitles");
+
+        String[] titles = ReadTitles.readTitles("./data/all_title.xml");
+        String places = "./data/all_places.xml";
+        String outputFile = "./data/places_highlighted.xml";
+
+        System.out.println("START - ReadPlacesCompareAndWrite");
+
+        ReadPlacesCompareAndWrite.readPlacesCompareAndWrite(places, titles, outputFile);
+
+        System.out.println("Finish");
     }
 }
